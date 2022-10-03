52afe557b0c3ed0d0611eaf3022bf23b415e93a4
Herselam Kahsay <hkahsay@c1r13s7.42lausanne.ch>
2022-10-03 20:27:02 +0200

    Create nodes to my push_swap

--- push_swap/push_swap/check_stack.c
+++ push_swap/push_swap/check_stack.c
@@ -1,38 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   build_stack.c                                      :+:      :+:    :+:   */
+/*   check_stack.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkahsay <hkahsay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 15:11:12 by hkahsay           #+#    #+#             */
-/*   Updated: 2022/09/30 16:36:24 by hkahsay          ###   ########.fr       */
+/*   Updated: 2022/10/03 20:11:09 by hkahsay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include<string.h>
-#include<stdarg.h>
-#include<stdio.h>
-#include<unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char const *s)
-{
-	while (s != NULL && *s)
-		ft_putchar(*s++);
-}
-
-int	ft_isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
-}
+// #include<string.h>
+// #include<stdarg.h>
+// #include<stdio.h>
+// #include<unistd.h>
 
 void	print_error(void)
 {
@@ -49,26 +31,18 @@ int	check_only_number(int argc, char **argv)
 	while (i < argc)
 	{
 		j = 0;
-		// if (argv[i][j] == "-")
-		// 	j++;
 		if (!ft_isdigit(argv[i][j]))
 			return (0);
 		while (ft_isdigit(argv[i][j]))
 			j++;
-		if (argv[i][j])
-			return (0);
 		i++;
 	}
 	return (0);
 }
 
-int	main(void)
-{
-	int		ag;
-	char	*av;
+// int	check_doubles(t_stack *a, int x)
+// {
+	
+// }
 
-	ag = 2;
-	av = "1 2 3 4";
-	printf("%d\n", check_only_number(ag, &av));
-	return (0);
-}
+
--- push_swap/push_swap/push_swap.h
+++ push_swap/push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hkahsay <hkahsay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 14:08:58 by hkahsay           #+#    #+#             */
-/*   Updated: 2022/09/30 16:23:13 by hkahsay          ###   ########.fr       */
+/*   Updated: 2022/10/03 18:58:01 by hkahsay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,35 @@
 # include<stdarg.h>
 # include<stdio.h>
 # include<unistd.h>
+# include<stdlib.h>
 
-typedef struct s_pslst
+typedef struct s_node
 {
 	int				x;
-	struct s_pslst	*next;
-	struct s_pslst	*prev;
-}t_pslst;
+	int				num;
+	int				i;
+	struct s_node	*next;
+	struct s_node	*prev;
+}t_node;
 
 typedef struct s_stack
 {
-	t_pslst	*head;
-	t_pslst	*end;
-	int		top;
+	int		a_len;
+	int		b_len;
+	t_node	*a_tail;
+	t_node	*b_tail;
+	t_node	*a_head;
+	t_node	*b_head;
 }t_stack;
+
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
 int		ft_isdigit(int c);
+void	insert_begnning(t_node **tail, int value);
+void	insert_end(t_node **head, int value);
+void	remove_node(t_node *node);
+void	init_node(t_node **tail, t_node **head, int value);
+
+void	print_error(void);
+
 #endif
